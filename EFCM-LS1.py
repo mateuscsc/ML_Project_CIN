23f49628af09fe3c1f18753eb5b9848e4a43746e
Rafael Santana <jrafaeldesantana@gmail.com>
2022-01-14 22:36:12 -0300

    dataset

--- EFCM-LS1.py
+++ EFCM-LS1.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 def generate_u_matrix (n, m):
     matrix = np.random.rand(n, m)
@@ -6,7 +7,7 @@ def generate_u_matrix (n, m):
 
     return u
 
-def efcm_ls1 (d, c, tu, tv, t=100, epsilon=10e-10):
+def efcm_ls1 (d, c, tu, tv, t=150, epsilon=10e-10):
     """
     EFCM-LS1 Algorithm
 
@@ -21,9 +22,9 @@ def efcm_ls1 (d, c, tu, tv, t=100, epsilon=10e-10):
     tv : int
         the relevance weight of the variables
     t : int
-        the maximum number of iterations (default value: 100)
+        the maximum number of iterations (default value: 150)
     epsilon: float
-        the stop condition parameter (default value: 1e-5)
+        the stop condition parameter (default value: 1e-10)
     """
 
     # Initialization
@@ -40,5 +41,26 @@ def efcm_ls1 (d, c, tu, tv, t=100, epsilon=10e-10):
 
         # Step 3: Assignment
 
-# For test only
-efcm_ls1(np.random.randint(0, 255, (100,5)), 7, 0, 0)
+
+def main():
+    df = pd.read_table('segmentation.data', skiprows=2, index_col=False, sep=",")
+
+    dataset1 = df.iloc[:,3:8]       # Shape
+    dataset2 = df.iloc[:,9:18]      # RGB
+    dataset3 = df.iloc[:,3:18]      # Shape + RGB
+
+    results_dataset1 = []
+    results_dataset2 = []
+    results_dataset3 = []
+
+    for i in range(150):
+        results_dataset1.append(efcm_ls1(dataset1, 7, 0, 0, 150, 10e-10))
+
+    for i in range(150):
+        results_dataset2.append(efcm_ls1(dataset2, 7, 0, 0, 150, 10e-10))
+
+    for i in range(150):
+        results_dataset3.append(efcm_ls1(dataset3, 7, 0, 0, 150, 10e-10))
+
+if __name__ == "__main__":
+    main()
