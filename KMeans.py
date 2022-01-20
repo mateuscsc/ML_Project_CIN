d7f559316912eb2525adf6b5715b86fb0f15b9ac
Jose Rafael <jrafaeldesantana@gmail.com>
2022-01-19 22:46:41 -0300

    Kmeans

--- KMeans.py
+++ KMeans.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 from sklearn.cluster import KMeans
-from matplotlib import pyplot as plt
+import sys
 
 def main():
     df = pd.read_table('segmentation.data', skiprows=3, index_col=False, sep=",")
@@ -10,15 +10,21 @@ def main():
     dataset2 = df.iloc[:,9:19].to_numpy()       # RGB
     dataset3 = df.iloc[:,3:19].to_numpy()       # Shape + RGB
 
-    results_dataset1 = []
-    results_dataset2 = []
-    results_dataset3 = []
+    best_dataset1 = { 'objective': sys.maxsize, 'kmeans': None, 'i': 0 }
+    best_dataset2 = { 'objective': sys.maxsize, 'kmeans': None, 'i': 0 }
+    best_dataset3 = { 'objective': sys.maxsize, 'kmeans': None, 'i': 0 }
 
     for i in range(50):
         km = KMeans(n_clusters=7, max_iter=150).fit(dataset1)
-        print("Labels: {} \nClusters: {}".format(km.labels_, km.cluster_centers_))
-        print()
 
+        if (km.inertia_ < best_dataset1['objective']):
+            best_dataset1['objective'] = km.inertia_
+            best_dataset1['kmeans'] = km
+            best_dataset1['i'] = i
+
+    print(best_dataset1['kmeans'].cluster_centers_.shape)
+
+    '''
     for i in range(50):
         km = KMeans(n_clusters=7, max_iter=150).fit(dataset2)
         print("Labels: {} \nClusters: {}".format(km.labels_, km.cluster_centers_))
@@ -26,6 +32,7 @@ def main():
     for i in range(50):
         km = KMeans(n_clusters=7, max_iter=150).fit(dataset3)
         print("Labels: {} \nClusters: {}".format(km.labels_, km.cluster_centers_))
+    '''
 
 if __name__ == "__main__":
     main()
