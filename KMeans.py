bdb008c685b6b3bb043a41664430ea7a5c1a742c
Jose Rafael <jrafaeldesantana@gmail.com>
2022-01-22 10:44:31 -0300

    Rand and f-measure

--- KMeans.py
+++ KMeans.py
@@ -13,11 +13,11 @@ def main():
     dataset2 = df.iloc[:,9:19].to_numpy()       # RGB
     dataset3 = df.iloc[:,3:19].to_numpy()       # Shape + RGB
 
-    best_dataset1 = { 'objective': sys.maxsize, 'crisp': None, 'fuzzy': None, 'mpc': None, 'pe': None, 'f-measure': None, 'rand': None }
-    best_dataset2 = { 'objective': sys.maxsize, 'crisp': None, 'fuzzy': None, 'mpc': None, 'pe': None, 'f-measure': None, 'rand': None }
-    best_dataset3 = { 'objective': sys.maxsize, 'crisp': None, 'fuzzy': None, 'mpc': None, 'pe': None, 'f-measure': None, 'rand': None }
+    best_dataset1 = { 'objective': sys.maxsize, 'crisp': None, 'fuzzy': None, 'mpc': None, 'pe': None }
+    best_dataset2 = { 'objective': sys.maxsize, 'crisp': None, 'fuzzy': None, 'mpc': None, 'pe': None }
+    best_dataset3 = { 'objective': sys.maxsize, 'crisp': None, 'fuzzy': None, 'mpc': None, 'pe': None }
 
-    for i in range(50):
+    for _ in range(50):
         _, u, _, _, jm, _, _ = fuzz.cmeans(dataset1.T, 7, 2, 0.005, 150)
 
         if (jm[-1] < best_dataset1['objective']):
@@ -25,7 +25,7 @@ def main():
             best_dataset1['crisp'] = u.T.argmax(axis=1)     # Get labels
             best_dataset1['objective'] = jm[-1]             # Get objective function value
 
-    for i in range(50):
+    for _ in range(50):
         _, u, _, _, jm, _, _ = fuzz.cmeans(dataset2.T, 7, 2, 0.005, 150)
 
         if (jm[-1] < best_dataset2['objective']):
@@ -33,7 +33,7 @@ def main():
             best_dataset2['crisp'] = u.T.argmax(axis=1)     # Get labels
             best_dataset2['objective'] = jm[-1]             # Get objective function value
 
-    for i in range(50):
+    for _ in range(50):
         _, u, _, _, jm, _, _ = fuzz.cmeans(dataset3.T, 7, 2, 0.005, 150)
 
         if (jm[-1] < best_dataset3['objective']):
@@ -42,24 +42,37 @@ def main():
             best_dataset3['objective'] = jm[-1]             # Get objective function value
     
     # Modified Partition Coefficient
+    # Between 0 and 1. 
+    # 0 = Maximum fuzziness
+    # 1 = Hard partition
     best_dataset1['mpc'] = MPC(best_dataset1['fuzzy'])
     best_dataset2['mpc'] = MPC(best_dataset2['fuzzy'])
     best_dataset3['mpc'] = MPC(best_dataset3['fuzzy'])
 
     # Partition Entropy
+    # Between 0 and log(C). 
+    # The closer the value of PE to 0, the crisper the clustering is. 
+    # The index value close to the upper bound indicates the absence of any
+    # clustering structure in the datasets or inability of the algorithm to extract it.
     best_dataset1['pe'] = PE(best_dataset1['fuzzy'])
     best_dataset2['pe'] = PE(best_dataset2['fuzzy'])
     best_dataset3['pe'] = PE(best_dataset3['fuzzy'])
 
     # Rand
-    best_dataset1['rand'] = adjusted_rand_score(best_dataset1['crisp'], )
-    best_dataset2['rand'] = adjusted_rand_score(best_dataset2['crisp'], )
-    best_dataset3['rand'] = adjusted_rand_score(best_dataset3['crisp'], )
+    # Between 0 and 1. 
+    # 0 = Random independently of the number of clusters and samples.
+    # 1 = When the clusterings are identical (up to a permutation).
+    rand_d1_d2 = adjusted_rand_score(best_dataset1['crisp'], best_dataset2['crisp']) 
+    rand_d1_d3 = adjusted_rand_score(best_dataset1['crisp'], best_dataset3['crisp']) 
+    rand_d2_d3 = adjusted_rand_score(best_dataset2['crisp'], best_dataset3['crisp'])
 
     # F-Measure
-    best_dataset1['f-measure'] = FMeasure(best_dataset1['crisp'], )
-    best_dataset2['f-measure'] = FMeasure(best_dataset2['crisp'], )
-    best_dataset3['f-measure'] = FMeasure(best_dataset3['crisp'], )
+    # Between 0 and 1.
+    # 0 = Worse
+    # 1 = Better
+    fm_d1_d2 = FMeasure(best_dataset1['crisp'], best_dataset2['crisp']) 
+    fm_d1_d3 = FMeasure(best_dataset1['crisp'], best_dataset3['crisp']) 
+    fm_d2_d3 = FMeasure(best_dataset2['crisp'], best_dataset3['crisp'])
 
     # Print results
     print("Dataset 1")
@@ -68,6 +81,19 @@ def main():
     print("MPC: {}\tPE: {}".format(best_dataset2['mpc'], best_dataset2['pe']))
     print("Dataset 3")
     print("MPC: {}\tPE: {}".format(best_dataset3['mpc'], best_dataset3['pe']))
+
+    print("\nAdjusted Rand Score")
+
+    print("Dataset 1 x Dataset 2: {}".format(rand_d1_d2))
+    print("Dataset 1 x Dataset 3: {}".format(rand_d1_d3))
+    print("Dataset 2 x Dataset 3: {}".format(rand_d2_d3))
+
+    print("\nF-Measure")
+
+    print("Dataset 1 x Dataset 2: {}".format(fm_d1_d2))
+    print("Dataset 1 x Dataset 3: {}".format(fm_d1_d3))
+    print("Dataset 2 x Dataset 3: {}".format(fm_d2_d3))
+    
 
 if __name__ == "__main__":
     main()
--- metrics/partition_entropy.py
+++ metrics/partition_entropy.py
@@ -2,6 +2,12 @@ import numpy as np
 
 """
 Baseado na página 55 da dissertação de Sara Inés Rizo Rodríguez
+
+The PE index is a scalar measure of the amount of fuzziness in a given U.
+The PE index values range in [0, log(C)], the closer the value of PE to 0,
+the crisper the clustering is. The index value close to the upper bound 
+indicates the absence of any clustering structure in the datasets or inability
+of the algorithm to extract it.
 """
 
 def PE(U):
